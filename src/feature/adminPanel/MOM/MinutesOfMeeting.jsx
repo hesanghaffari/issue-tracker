2f506a0d57a01c88f0092d55f63c4c8b7c4c84db
Hesan <hesan9663@gmail.com>
2024-10-09 14:50:41 +0330

    api

--- src/feature/adminPanel/MOM/MinutesOfMeeting.jsx
+++ src/feature/adminPanel/MOM/MinutesOfMeeting.jsx
@@ -5,28 +5,36 @@ import ListChildTable from "./ListMomTable";
 import { useAddMom } from "./useAddMom";
 import Search from "../../../ui/Search";
 
-function ChildManagment() {
+function ChildManagement() {
   const { addMom, isPending } = useAddMom();
 
   return (
     <div>
-      <Search />
-      <Modal>
-        <Modal.Open opens="AddMomForm">
-          <Button>ثبت صورت جلسه جدید</Button>
-        </Modal.Open>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Search />
+        <Modal>
+          <Modal.Open opens="AddMomForm">
+            <Button>ثبت صورت جلسه جدید</Button>
+          </Modal.Open>
 
-        <Modal.Window name="AddMomForm">
-          <AddMomForm
-            onCloseModal={() => close()}
-            addAdmin={addMom}
-            isPending={isPending}
-          />
-        </Modal.Window>
-      </Modal>
+          <Modal.Window name="AddMomForm">
+            <AddMomForm
+              onCloseModal={() => close()}
+              addAdmin={addMom}
+              isPending={isPending}
+            />
+          </Modal.Window>
+        </Modal>
+      </div>
       <ListChildTable />
     </div>
   );
 }
 
-export default ChildManagment;
+export default ChildManagement;
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const mainURL = "http://localhost:3000";
-// https://itk.maynd.ir
+const mainURL = "https://itk.maynd.ir/api";
+// https://itk.maynd.ir/api
 export async function signup({
   fullname,
   email,
